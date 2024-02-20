c8bfead74665e1839abe8b907e65221795060ac9
Natnael Biruk <141657778+Natib22@users.noreply.github.com>
2024-02-20 18:51:25 +0300

    Add solution for Time Needed to Buy Tickets

---
+++ A2SV/Queue/leetcode/time-needed-to-buy-tickets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        ans = 0
+        i = 0 
+        while tickets[k] != 0:
+            if tickets[i]!= 0:
+                tickets[i]-=1
+                ans+=1
+            i+=1
+            if i == len(tickets):
+                i=0
+        return ans
+            
+
+        

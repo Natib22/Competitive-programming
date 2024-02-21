c4184a26ace6aa531e91a7ee56c000a64e8a9cbd
Natnael Biruk <141657778+Natib22@users.noreply.github.com>
2024-02-21 17:40:21 +0300

    Add solution for Valid Palindrome

---
+++ A2SV/Array/leetcode/valid-palindrome.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        string  = ""
+        for i in s:
+            if i.isalnum():
+                string+= i.lower()
+
+        l , r = 0  , len(string)-1
+ 
+        while l < r:
+            if string[l] != string[r]:
+                return False
+            l+=1
+            r-=1
+        
+        return True        

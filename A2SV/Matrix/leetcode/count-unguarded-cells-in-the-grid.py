4a2e5c5e7b3713d012a4502ab8957f84244f2b89
Natnael Biruk <141657778+Natib22@users.noreply.github.com>
2024-03-02 10:24:15 +0300

    Add solution for Count Unguarded Cells in the Grid

--- A2SV/Matrix/leetcode/count-unguarded-cells-in-the-grid.py
+++ A2SV/Matrix/leetcode/count-unguarded-cells-in-the-grid.py
@@ -1,4 +1,4 @@
-from typing import List
+
 
 class Solution:
     def countUnguarded(self, m: int, n: int, guards: List[List[int]], walls: List[List[int]]) -> int:
@@ -52,6 +52,11 @@ class Solution:
 
        
         return sum(row.count(0) for row in arr)
+
+
+
+
+        
 
 
 
